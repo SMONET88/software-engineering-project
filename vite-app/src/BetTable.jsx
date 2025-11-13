58d1a95d7b99977eab9eb68156cb2277d5818f6a
Samantha Phillips <samanthaphillips886@gmail.com>
2025-11-13 13:24:02 -0600

    updated bet table

--- vite-app/src/BetTable.jsx
+++ vite-app/src/BetTable.jsx
@@ -1,22 +1,105 @@
 import "./App.css";
-import CalculateBet from "./CalculateBet";
 import { useState, useEffect } from "react";
-import HandleBet from "./HandleBet";
+import sample_data from "./sampleData";
 
 const BetTable = ({ game, formatTime, betType }) => {
-  const [betTeam, setName] = useState("");
+  const [betTeam, setBetTeam] = useState("");
   const [disabledGame, setDisabledGame] = useState(game.id);
   const [showTotalTable, setShowTotalTable] = useState(false);
+  const [homeBetInput, setHomeBetInput] = useState("");
+  const [awayBetInput, setAwayBetInput] = useState("");
+  const [tableOdds, setTableOdds] = useState("");
+  const [totalProfit, setTotalProfit] = useState([]);
 
+  // immutable update
+  const addProfit = (newProfitAddition) => {
+    setTotalProfit((prev) => [...prev, newProfitAddition]);
+  };
 
+  let oddsDictionary = {};
 
-  const onClick = (teamName) => {
-    setName(teamName);
-    setDisabledGame(game.id);
+  sample_data.forEach((game) => {
+    const homeTeam = game.home_team;
+    const awayTeam = game.away_team;
+
+    game.bookmakers?.forEach((bookmaker) => {
+      const h2h = bookmaker.markets.find((m) => m.key === "h2h");
+      const spread = bookmaker.markets.find((m) => m.key === "spreads");
+      const totals = bookmaker.markets.find((m) => m.key === "totals");
+
+      const totalsOver = totals?.outcomes.find((n) => n.name === "Over");
+      const totalsUnder = totals?.outcomes.find((n) => n.name === "Under");
+
+      // Home team odds
+      oddsDictionary[homeTeam] = {
+        h2hOdds: h2h?.outcomes[0]?.price,
+        h2hLine: null,
+
+        spreadOdds: spread?.outcomes[0]?.price,
+        spreadLine: spread?.outcomes[0]?.point,
+
+        totalOddsOver: totalsOver.price,
+        totalLineOver: totalsOver.point,
+
+        totalOddsUnder: totalsUnder.price,
+        totalLineUnder: totalsUnder.point,
+      };
+
+      // Away team odds
+      oddsDictionary[awayTeam] = {
+        h2hOdds: h2h?.outcomes[1]?.price,
+        h2hLine: null,
+
+        spreadOdds: spread?.outcomes[1]?.price,
+        spreadLine: spread?.outcomes[1]?.point,
+
+        totalOddsOver: totalsOver.price,
+        totalLineOver: totalsOver.point,
+
+        totalOddsUnder: totalsUnder.price,
+        totalLineUnder: totalsUnder.point,
+      };
+    });
+  });
+  console.log(`HERE HER E88: ${betType}`);
+
+  const profitCalculation = (betInput, odds) => {
+    console.log(`team: ${betTeam}, odds: ${odds}`);
+    const bet = Number(betInput);
+    if (typeof bet !== "number" || isNaN(bet)) {
+      return 0;
+    } else if (odds === 100) {
+      return bet * 2;
+    } else if (odds > 0) {
+      return ((odds / 100) * bet).toFixed(2);
+    } else if (odds < 0) {
+      return ((100 / Math.abs(odds)) * bet).toFixed(2);
+    }
+  };
+
+  const onClickHandle = (teamName, gameId) => {
+    console.log(`xxx ${gameId}`);
+    setBetTeam(teamName);
+    setDisabledGame(gameId);
+
+    if (teamName === game.home_team) {
+      addProfit(homeProfit);
+    } else if (teamName === game.away_team) {
+      addProfit(awayProfit);
+    }
+    // const userBet = {
+    //   gameId: game.id,
+    //   BetType: betType,
+    //   team: teamName,
+    //   line: odds,
+    //   isOver: true,
+    //   amount: betInput,
+    // };
+    console.log(`total prof ${totalProfit}`);
   };
 
   useEffect(() => {
-    setName('');
+    setBetTeam("");
     setDisabledGame(true);
 
     if (betType === "Totals (over/under)") {
@@ -24,13 +107,28 @@ const BetTable = ({ game, formatTime, betType }) => {
     } else {
       setShowTotalTable(false);
     }
+
+    if (betType.includes("moneyline")) {
+      setTableOdds("h2hOdds");
+    } else if (betType.includes("spread")) {
+      setTableOdds("spreadOdds");
+    } else if (betType.includes("over/under")) {
+      setTableOdds("totalOddsOver");
+    }
   }, [betType]);
+
+  const homeProfit = profitCalculation(
+    homeBetInput,
+    oddsDictionary[game.home_team][tableOdds],
+  );
+  const awayProfit = profitCalculation(
+    awayBetInput,
+    oddsDictionary[game.away_team][tableOdds],
+  );
 
   return (
     <>
-
       {!showTotalTable ? (
-
         <div key={game.id} className="rounded-div">
           <table>
             <thead>
@@ -44,16 +142,59 @@ const BetTable = ({ game, formatTime, betType }) => {
               <tr>
                 <td style={{ fontWeight: "bold" }}>{game.home_team}</td>
                 <td>
-                  <HandleBet game={game} betType={betType} isHome={true} />
+                  <div
+                    className="table-data"
+                    style={{
+                      display: "flex",
+                      gap: "8px",
+                      alignItems: "center",
+                    }}
+                  >
+                    <input
+                      type="text"
+                      className="input"
+                      value={homeBetInput}
+                      onChange={(e) => setHomeBetInput(e.target.value)}
+                    />
+                    <button
+                      className="betButton"
+                      onClick={() => onClickHandle(game.home_team, game.id)}
+                      disabled={disabledGame === game.id}
+                    >
+                      Place
+                    </button>
+                  </div>
+                  <div className="box">{homeProfit}</div>
                 </td>
-
               </tr>
 
               {/* Away team row */}
               <tr>
                 <td style={{ fontWeight: "bold" }}>{game.away_team}</td>
                 <td>
-                  <HandleBet game={game} betType={betType} isHome={false} />
+                  <div
+                    className="table-data"
+                    style={{
+                      display: "flex",
+                      gap: "8px",
+                      alignItems: "center",
+                    }}
+                  >
+                    <input
+                      type="text"
+                      className="input"
+                      value={awayBetInput}
+                      onChange={(e) => setAwayBetInput(e.target.value)}
+                    />
+                    <button
+                      className="betButton"
+                      onClick={() => onClickHandle(game.away_team, game.id)}
+                      disabled={disabledGame === game.id}
+                    >
+                      Place
+                    </button>
+                  </div>
+                  <div className="box">{awayProfit}</div>
                 </td>
               </tr>
               <tr>
@@ -64,49 +205,85 @@ const BetTable = ({ game, formatTime, betType }) => {
             </tbody>
           </table>
         </div>
-
-      ) :
+      ) : (
         <div key={game.id} className="rounded-div">
           <table>
             <thead>
               <tr>
-                <td>{game.home_team}</td>
-                <td>Over</td>
-                <td>{<HandleBet game={game} betType={betType} isHome={true} />}</td>
+                <th>{formatTime(game.commence_time)}</th>
+                <th>{betType} Bet</th>
               </tr>
+              <td style={{ fontWeight: "bold" }}>{game.home_team}</td>
+              <td>Over</td>
+              <td>
+                <div
+                  className="table-data"
+                  style={{
+                    display: "flex",
+                    gap: "8px",
+                    alignItems: "center",
+                  }}
+                >
+                  <input
+                    type="text"
+                    className="input"
+                    value={homeBetInput}
+                    onChange={(e) => setHomeBetInput(e.target.value)}
+                  />
+                  <button
+                    className="betButton"
+                    onClick={() => onClickHandle(game.home_team, game.id)}
+                    disabled={disabledGame === game.id}
+                  >
+                    Place
+                  </button>
+                </div>
+                <div className="box">{homeProfit}</div>
+              </td>
               <tr>
                 <td>vs. </td>
                 <td>{game.bookmakers[0].markets[2].outcomes[0].point}</td>
-
-
               </tr>
               <tr>
-                <td>{game.away_team}</td>
+                <td style={{ fontWeight: "bold" }}>{game.away_team}</td>
                 <td>Under</td>
-                <td>{<HandleBet game={game} betType={betType} isHome={false} />}</td>
-
-
+                <td>
+                  <div
+                    className="table-data"
+                    style={{
+                      display: "flex",
+                      gap: "8px",
+                      alignItems: "center",
+                    }}
+                  >
+                    <input
+                      type="text"
+                      className="input"
+                      value={awayBetInput}
+                      onChange={(e) => setAwayBetInput(e.target.value)}
+                    />
+                    <button
+                      className="betButton"
+                      onClick={() => onClickHandle(game.away_team, game.id)}
+                      disabled={disabledGame === game.id}
+                    >
+                      Place
+                    </button>
+                  </div>
+                  <div className="box">{awayProfit}</div>
+                </td>
               </tr>
-
-
-
+              <tr>
+                <td>
+                  <h2>{`You bet on ${betTeam}`}</h2>
+                </td>
+              </tr>
             </thead>
-
-
-
           </table>
         </div>
-
-
-
-
-      }
+      )}
     </>
   );
 };
-
-
-
-
 
 export default BetTable;
